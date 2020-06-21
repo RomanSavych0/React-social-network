67c21d0a9a62782fd2e55e79ad4e561b0df55008
Tweenlight <nomber491@gmail.com>
2020-06-21 17:24:07 +0300

    .

--- src/components/profile/ProfileContainer.jsx
+++ src/components/profile/ProfileContainer.jsx
@@ -10,6 +10,7 @@ class ProfileContainer extends React.Component {
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`).then(response => {
 
             this.props.setUserProfile(response.data);
+
         });
 
     }
